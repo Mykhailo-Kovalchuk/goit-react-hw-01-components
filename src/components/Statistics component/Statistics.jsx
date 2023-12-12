a4fcb6573a7d61a6c8a80f5c620a916a68765af1
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-12-12 09:56:44 +0100

    goit-react-hw-01-commit-9

--- src/components/Statistics component/Statistics.jsx
+++ src/components/Statistics component/Statistics.jsx
@@ -1,23 +1,28 @@
 // import React from 'react'
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
 
-const Statistics = ({ title = "", stats }) => {
-
-    const statElement = stats.map((el) => (
-<li className={css.item} key={el.id}>
-<span className={css.label}>{el.label}</span>
-<span className={css.percentage}>{el.percentage}%</span>
-</li>))
-
+const Statistics = ({ title = '', stats }) => {
+  const statElement = stats.map(el => (
+    <li
+      className={css.item}
+      key={el.id}
+      style={{ backgroundColor: '#' + randomColor() }}
+    >
+      <span className={css.label}>{el.label}</span>
+      <span className={css.percentage}>{el.percentage}%</span>
+    </li>
+  ));
 
   return (
-<section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
-  <ul className={css.statlist}>
-  {statElement}
-  </ul>
-  </section>
-  )
-}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statlist}>{statElement}</ul>
+    </section>
+  );
+};
 
-export {Statistics};
+export { Statistics };
+
+function randomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
