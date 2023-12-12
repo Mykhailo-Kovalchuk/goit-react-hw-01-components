9fda7ae707dfee1f5fbdfe7c98ea6b5df506b95b
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-12-12 10:11:40 +0100

    goit-react-hw-01-commit-10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,51 @@
-import {Profile} from './Profile component/Profile' // Імпорт компоненту Profile
+import { Profile } from './Profile component/Profile'; // Імпорт компоненту Profile
 import user from './Profile component/user.json'; // імпорт даних юзера з файлу джейсон (підозрюю що замість файлу по нормальному будуть приходити дані з сервера)
 
-import {Statistics} from './Statistics component/Statistics'
-import statisticData from './Statistics component/data.json'
+import { Statistics } from './Statistics component/Statistics';
+import statisticData from './Statistics component/data.json';
 
-import {FriendList} from './FriendList conponent/FriendList'
-import friends from './FriendList conponent/friends.json'
+import { FriendList } from './FriendList conponent/FriendList';
+import friends from './FriendList conponent/friends.json';
 
 import { TransactionHistory } from './Transactions component/Transactions.jsx';
-import transactions from './Transactions component/transactions.json'
-
+import transactions from './Transactions component/transactions.json';
 
 export const App = () => {
-
   return (
     // Не збуваємо про занальний div або порожній тег <>
-////////////////// 1. //На майбутнє, тут буде HEADER який буде створюватись теж як реакт компонент
+    ////////////////// 1. //На майбутнє, тут буде HEADER який буде створюватись теж як реакт компонент
 
-/////////////// 2. // Тут типу main, теж як компонент але він буде інтерактивний (тобто буде змінюватись)
+    /////////////// 2. // Тут типу main, теж як компонент але він буде інтерактивний (тобто буде змінюватись)
 
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
 
-      <p> React homework template</p>
-
-         <Profile 
-         username={user.username}
-         tag={user.tag}
-         location={user.location}
-         avatar={user.avatar}
-         stats={user.stats}
-         />
-
-          <Statistics 
-          title="Upload stats"
-          stats={statisticData} 
-          />
+      <Statistics
+        // title="Upload stats"
+        stats={statisticData}
+      />
 
       <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />
     </div>
 
-
-      
-      ///////////// 3. На майбутнє, тут буде FOOTER який буде створюватись теж як реакт компонент
-
+    ///////////// 3. На майбутнє, тут буде FOOTER який буде створюватись теж як реакт компонент
   );
 };
--- src/components/FriendList conponent/FriendList.jsx
+++ src/components/FriendList conponent/FriendList.jsx
@@ -1,21 +1,19 @@
 // import React from 'react'
-import css from './FrientList.module.css'
+import css from './FrientList.module.css';
+import { FriendListItem } from '../FriendListItem component/FriendListItem.jsx';
 
-const FriendList = ({friends}) => {
-
-  const friendsNewArray = friends.map(el => (
-<li className={css.item} key={el.id}>
-  <span className={css.status}>{el.isOnline ? "🟢" : "🔴"}</span>
-  <img className={css.avatar} src={el.avatar} alt="User avatar" width="48" />
-  <p className={css.name}>{el.name}</p>
-</li>
-  ))
-
+const FriendList = ({ friends }) => {
   return (
-<ul className={css.friendlist}>
-{friendsNewArray}
-</ul>
-  )
-}
-export  {FriendList}  
-
+    <ul className={css.friendlist}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+export { FriendList };
